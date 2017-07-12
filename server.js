122aa694edda59e03f143d7a72f91305066b8621
Visnu Pitiyanuvath <visnup@gmail.com>
2017-07-12 01:33:48 -0700

    include index

--- server.js
+++ server.js
@@ -9,14 +9,20 @@ let server = http.createServer((req, res) => {
   let date = moment().subtract(1, 'month').format('YYYY/MM'),
       lastIssue = `https://harpers.org/archive/${date}`
 
-  load(lastIssue)
-    .then($ => load($('a[href*="findings"]').attr('href')))
+  request(lastIssue)
     .then($ => {
-      let text = $('.articlePost > p').text()
-      let sentences = tokenizer.sentences(text)
-
+      return Promise.all([
+        request($('a[href*="/findings"]').attr('href'))
+          .then($ => tokenizer.sentences($('.articlePost > p').text()))
+        ,
+        request($('a[href*="/harpers-index"]').attr('href'))
+          .then($ => $('.articlePost > p[style]:has(strong)').map((i, p) => $(p).text()).get())
+      ])
+    })
+    .then(results => {
+      results = Array.prototype.concat(...results)
       res.writeHead(200, { 'Content-Type': 'text/plain' })
-      res.end(sentences.join('\n') + '\n')
+      res.end(results.join('\n') + '\n')
     })
 })
 
@@ -25,7 +31,7 @@ server.listen(port, () => {
   console.log(`Listening on 0.0.0.0:${port}`)
 })
 
-function load(url) {
+function request(url) {
   let agent = new https.Agent({ rejectUnauthorized: false })
   return fetch(url, { agent })
     .then(res => res.text())
