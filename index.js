7960f1b14060256b98ddc13842093f4aa67189be
Visnu Pitiyanuvath <visnup@gmail.com>
2020-04-06 15:22:37 -0700

    site updated

--- index.js
+++ index.js
@@ -1,34 +1,30 @@
-const cheerio = require('cheerio')
-const fetch = require('node-fetch')
-const https = require('https')
-const moment = require('moment')
-const tokenizer = require('sbd')
+const cheerio = require("cheerio");
+const fetch = require("node-fetch");
+const https = require("https");
+const tokenizer = require("sbd");
 
 module.exports = (req, res) => {
-  let date = moment().subtract(1, 'month').format('YYYY/MM'),
-      lastIssue = `https://harpers.org/archive/${date}/`
-
-  request(lastIssue)
-    .then($ => {
-      return Promise.all([
-        request($('a[href*="/findings-"]').attr('href'))
-          .then($ => tokenizer.sentences($('.articlePost > p').text()))
-        ,
-        request($('a[href*="/harpers-index-"]').attr('href'))
-          .then($ => $('.articlePost > p[style]').map((i, p) => $(p).text()).get())
-      ])
-    })
-    .then(results => Array.prototype.concat(...results))
-    .then(results => {
-      res.writeHead(200, { 'Content-Type': 'text/plain' })
-      res.end(results.join('\n') + '\n')
-    })
-}
+  return Promise.all([
+    request("https://harpers.org/sections/findings/")
+      .then(($) => request($(".card a").attr("href")))
+      .then(($) => tokenizer.sentences($(".wysiwyg-content > p").text())),
+    request("https://harpers.org/harpers-index/").then(($) =>
+      $(".index-statement > p")
+        .map((i, p) => $(p).text())
+        .get()
+    ),
+  ])
+    .then((results) => Array.prototype.concat(...results))
+    .then((results) => {
+      res.writeHead(200, {"Content-Type": "text/plain"});
+      res.end(results.join("\n") + "\n");
+    });
+};
 
 async function request(url) {
-  console.log(url)
-  const agent = new https.Agent({ rejectUnauthorized: false })
-  const res = await fetch(url, { agent })
-  const text = await res.text()
-  return cheerio.load(text)
+  console.log(url);
+  const agent = new https.Agent({rejectUnauthorized: false});
+  const res = await fetch(url, {agent});
+  const text = await res.text();
+  return cheerio.load(text);
 }
