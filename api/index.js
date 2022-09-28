097f1360e96c59ba758082fdf6882b7089189ae3
Visnu Pitiyanuvath <visnup@gmail.com>
2022-09-27 21:44:35 -0700

    esm

--- api/index.js
+++ api/index.js
@@ -1,10 +1,9 @@
-const cheerio = require("cheerio");
-const fetch = require("node-fetch");
-const https = require("https");
-const tokenizer = require("sbd");
+import { load } from "cheerio";
+import fetch from "node-fetch";
+import tokenizer from "sbd";
 
 export default async function index(req, res) {
-  return Promise.all([
+  const results = await Promise.all([
     request("https://harpers.org/sections/findings/")
       .then(($) => request($(".card a").attr("href")))
       .then(($) => tokenizer.sentences($(".wysiwyg-content > p").text())),
@@ -13,18 +12,14 @@ export default async function index(req, res) {
         .map((_, s) => $("> p", s).text() + $("> span", s).text().trim())
         .get()
     ),
-  ])
-    .then((results) => Array.prototype.concat(...results))
-    .then((results) => {
-      res.writeHead(200, { "Content-Type": "text/plain" });
-      res.end(results.join("\n") + "\n");
-    });
+  ]);
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end(results.flat().join("\n") + "\n");
 }
 
 async function request(url) {
   console.log(url);
-  const agent = new https.Agent({ rejectUnauthorized: false });
-  const res = await fetch(url, { agent });
+  const res = await fetch(url);
   const text = await res.text();
-  return cheerio.load(text);
+  return load(text);
 }
