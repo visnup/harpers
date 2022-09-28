5333d03dac9a8da5d41b748e29c7c404f984310f
Visnu Pitiyanuvath <visnup@gmail.com>
2022-09-27 21:38:05 -0700

    wip

--- api/index.js
+++ api/index.js
@@ -3,7 +3,7 @@ const fetch = require("node-fetch");
 const https = require("https");
 const tokenizer = require("sbd");
 
-module.exports = (req, res) => {
+export default async function index(req, res) {
   return Promise.all([
     request("https://harpers.org/sections/findings/")
       .then(($) => request($(".card a").attr("href")))
@@ -16,15 +16,15 @@ module.exports = (req, res) => {
   ])
     .then((results) => Array.prototype.concat(...results))
     .then((results) => {
-      res.writeHead(200, {"Content-Type": "text/plain"});
+      res.writeHead(200, { "Content-Type": "text/plain" });
       res.end(results.join("\n") + "\n");
     });
-};
+}
 
 async function request(url) {
   console.log(url);
-  const agent = new https.Agent({rejectUnauthorized: false});
-  const res = await fetch(url, {agent});
+  const agent = new https.Agent({ rejectUnauthorized: false });
+  const res = await fetch(url, { agent });
   const text = await res.text();
   return cheerio.load(text);
 }
